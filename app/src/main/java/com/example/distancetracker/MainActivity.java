695ca5aa36e428b45b0650dc47df65f76887e4d8
Saurer1994 <markussaurer@gmx.at>
2019-11-07 21:24:28 +0100

    QR Code Scanner got his own activity and now return the numberplate via intent

--- app/src/main/java/com/example/distancetracker/MainActivity.java
+++ app/src/main/java/com/example/distancetracker/MainActivity.java
@@ -38,14 +38,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-import static android.Manifest.permission.CAMERA;
-
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-
-    //For QR Scanner new
-    private static final int REQUEST_CAMERA = 1;
-    private ZXingScannerView mScannerView;
+public class MainActivity extends AppCompatActivity  {
 
     private LocationManager locationMangaer = null;
     private LocationListener locationListener = null;
@@ -62,7 +55,6 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     String latitude = "";
     String location = "";
     String altitude = "";
-
 
     double longOld = 0;
     double latOld = 0;
@@ -89,25 +81,18 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         BtnStart = (Button) findViewById(R.id.btnStart);
         BtnStop = (Button) findViewById(R.id.btnStop);
 
-        //--------- QR Scanner
-        mScannerView = new ZXingScannerView(MainActivity.this);
-        setContentView(mScannerView);
-        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
-            if (checkPermission()) {
-                Toast.makeText(getApplicationContext(), "Permission already granted", Toast.LENGTH_LONG).show();
-
-            } else {
-                requestPermission();
-            }
+        //Waiting for car licence plate
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            v_gps_status.setText(extras.getString("QR"));
         }
-        //--------- QR Scanner
+
 
         BtnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                Intent QRIntent =  new Intent(MainActivity.this, QRScannerActivity.class);
+                startActivity(QRIntent);
             }
         });
 
@@ -144,106 +129,6 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         });
         locationMangaer = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
-
-    //--------- QR Scanner
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
-            if (checkPermission()) {
-                if(mScannerView == null) {
-                    mScannerView = new ZXingScannerView(this);
-                    setContentView(mScannerView);
-                }
-                mScannerView.setResultHandler(this);
-                mScannerView.startCamera();
-            } else {
-                requestPermission();
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mScannerView.stopCamera();
-    }
-
-    private boolean checkPermission() {
-        return ( ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA ) == PackageManager.PERMISSION_GRANTED);
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CAMERA:
-                if (grantResults.length > 0) {
-
-                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if (cameraAccepted){
-                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
-                    }else {
-                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if (shouldShowRequestPermissionRationale(CAMERA)) {
-                                showMessageOKCancel("You need to allow access to both the permissions",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                    requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
-                                                }
-                                            }
-                                        });
-                                return;
-                            }
-                        }
-                    }
-                }
-                break;
-        }
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(MainActivity.this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
-    @Override
-    public void handleResult(Result rawResult) {
-        final String result = rawResult.getText();
-        Log.d("QRCodeScanner", rawResult.getText());
-        Log.d("QRCodeScanner", rawResult.getBarcodeFormat().toString());
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                mScannerView.resumeCameraPreview(MainActivity.this);
-            }
-        });
-        builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(result));
-                startActivity(browserIntent);
-            }
-        });
-        builder.setMessage(rawResult.getText());
-        AlertDialog alert1 = builder.create();
-        alert1.show();
-    }
-    //--------- QR Scanner
 
 
     private class GPSTracker implements LocationListener {
